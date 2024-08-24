ddf4f62201f1b6ee09a3d64eb26efdbf4d6cfeee
rizki <rizkiadnanfutuh@gmail.com>
2024-08-25 03:51:30 +0700

    feat: add header component

--- lib/screens/sections/home/home_page.dart
+++ lib/screens/sections/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:e_book_app/themes.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,8 +6,66 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home Page'),
+    Widget header() {
+      return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 30),
+        child: Row(
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/profile.png',
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hello Adnan,',
+                  style: semiBold16.copyWith(color: blackColor),
+                ),
+                Text(
+                  'Good Morning',
+                  style: regular14.copyWith(color: greyColor),
+                ),
+              ],
+            ),
+            const Spacer(),
+            Image.asset(
+              'assets/icons/menu.png',
+              width: 18,
+              height: 14,
+            )
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroungColor,
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 30),
+            decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+              ),
+            ),
+            child: header(),
+          ),
+        ],
+      ),
     );
   }
 }
