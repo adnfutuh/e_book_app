1dae9c66fde6d92629333ddc6795fce1f6d44a5d
rizki <rizkiadnanfutuh@gmail.com>
2024-08-24 16:15:26 +0700

    feat: added themes.dart

---
+++ lib/themes.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color backgroungColor = const Color(0xFFE5E5E5);
+Color whiteColor = const Color(0xFFFFFFFF);
+Color blackColor = const Color(0xFF2D2D2D);
+Color blackColor2 = const Color(0xFF000000);
+Color greyColor = const Color(0xFFBDBDBD);
+Color greyColorRecentBook = const Color(0xFFAFAFAF);
+Color greyColorSearchField = const Color(0xFFF8F8F8);
+Color greyColorInfo = const Color(0xFFF7F7F7);
+Color greenColor = const Color(0xFF098B5C);
+Color dividerColor = const Color(0xFF6B6B6B);
+
+TextStyle semiBold20 = GoogleFonts.poppins(fontSize: 20, fontWeight: semiBold);
+TextStyle semiBold16 = GoogleFonts.poppins(fontSize: 16, fontWeight: semiBold);
+TextStyle semiBold14 = GoogleFonts.poppins(fontSize: 14, fontWeight: semiBold);
+TextStyle semiBold12 = GoogleFonts.poppins(fontSize: 12, fontWeight: semiBold);
+
+TextStyle meduim14 = GoogleFonts.poppins(fontSize: 14, fontWeight: meduim);
+TextStyle meduim12 =
+    GoogleFonts.poppins(color: dividerColor, fontSize: 12, fontWeight: meduim);
+TextStyle meduim10 = GoogleFonts.poppins(fontSize: 10, fontWeight: meduim);
+
+TextStyle regular12 =
+    GoogleFonts.poppins(color: greyColor, fontSize: 12, fontWeight: regular);
+TextStyle regular14 = GoogleFonts.poppins(fontSize: 14, fontWeight: regular);
+
+FontWeight regular = FontWeight.w400;
+FontWeight meduim = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
